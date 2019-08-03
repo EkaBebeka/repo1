2758b3a6c37698a3d40a09dccbe487a9e749137a
EkaBebeka <52282459+EkaBebeka@users.noreply.github.com>
2019-08-03 20:16:15 +0300

    Задание 1

---
+++ task.js
@@ -0,0 +1,60 @@
+
+
+function calculateQuadraticEquation(){
+    let a = +window.a.value;
+    let b = +window.b.value;
+    let c = +window.c.value;
+    let result = getResult(a,b,c);
+    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+    let span = window.result;
+    span.textContent = "х = "+result;
+}
+
+function getResult(a,b,c){
+    // код для задачи №1 писать здесь
+    //return x;
+
+"use strict"
+let x;
+let d;
+return x;
+d = Math.pow(b, 2) - (4 * a * c);
+if (d>0){
+x1 = ((-1) * b + Math.sqrt(d)) / 2 * a;
+x2 = ((-1) * b - Math.sqrt(d)) / 2 * a;
+console.log("x1 = " + x1 + " x2 = " + x2);
+}
+else if (d == 0){
+x1 = (-1) * b;
+console.log("x1, x2 = " + x1);
+}
+else
+console.log("Ошибка. D меньше нуля");
+}
+    
+}
+
+
+function calculateDrinkTask(){
+    let name = window.personName.value;
+    let dateOfBirthday = new Date(window.dateOfBirthday.value);
+    let drink = askDrink(name, dateOfBirthday);
+    window.drink.textContent = drink;
+}
+
+function askDrink(name,dateOfBirthday){
+    // код для задачи №2 писать здесь
+    //console.log(result)
+    //return result;
+}
+
+function calculateAverageRating(){
+    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
+    let averageMark = getAverageMark(marks);
+    window.averageMark.textContent = averageMark;
+}
+
+function getAverageMark(marks){
+    // код для задачи №3 писать здесь
+    //return averageMark;
+}
