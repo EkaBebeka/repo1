8b3847ab8b68cf24ad6f1df3fb39504159fad888
EkaBebeka <52282459+EkaBebeka@users.noreply.github.com>
2019-08-09 23:29:10 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -15,23 +15,23 @@ function getResult(a,b,c){
     //return x;
 
 "use strict"
-let x;
+let x = [];
 let d;
-return x;
+
 d = Math.pow(b, 2) - (4 * a * c);
 if (d>0){
-x1 = ((-1) * b + Math.sqrt(d)) / 2 * a;
-x2 = ((-1) * b - Math.sqrt(d)) / 2 * a;
+x1 = (-b + Math.sqrt(d)) / 2 * a;
+x2 = (-b - Math.sqrt(d)) / 2 * a;
 console.log("x1 = " + x1 + " x2 = " + x2);
 }
 else if (d == 0){
-x1 = (-1) * b;
+x1 = (-b) / 2 * a;;
 console.log("x1, x2 = " + x1);
 }
 else
 console.log("Ошибка. D меньше нуля");
 }
-    
+return x;   
 }
 
 
